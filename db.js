59f2b0d85db7bc04eb96ffae11ce953561ec12f7
EDA <eda@mac-mini-1.local>
2017-05-11 11:56:18 +1200

    Stretch tasks completed

--- db.js
+++ db.js
@@ -1,13 +1,35 @@
-
 module.exports = {
   getUser: getUser,
-  getUsers: getUsers
+  getUsers: getUsers,
+  addUser,
+  editUser,
+  deleteUser
 }
 
-function getUsers (connection) {
+function getUsers(connection) {
   return connection('users').select()
 }
 
-function getUser (id, connection) {
+function getUser(id, connection) {
   return connection('users').where('id', id)
+}
+
+function addUser(name, email, connection) {
+  return connection('users')
+    .insert({
+      name: name,
+      email: email
+    })
+}
+
+function editUser(id, edited, connection) {
+  return connection('users')
+    .where('id', id)
+    .update(edited)
+}
+
+function deleteUser(id, connection) {
+  return connection('users')
+    .where('id', id)
+    .del()
 }
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,71 @@ var router = express.Router()
 var db = require('../db')
 
 router.get('/', function (req, res) {
+  res.redirect('/users')
+})
+
+router.get('/users', function (req, res) {
   db.getUsers(req.app.get('connection'))
     .then(function (users) {
-      res.render('index', { users: users })
+      res.render('index', {
+        users: users
+      })
     })
     .catch(function (err) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+router.get('/users/new', function (req, res) {
+  res.render('new')
+})
+
+router.post('/users/submit', function (req, res) {
+  const name = req.body.name
+  const email = req.body.email
+  db.addUser(name, email, req.app.get('connection'))
+    .then(function () {
+      res.redirect('/users')
+    })
+})
+
+router.get('/users/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db.getUser(id, req.app.get('connection'))
+    .then((result) => {
+      // console.log(result)
+      res.render('edit', result[0])
+    })
+})
+
+router.post('/users/submit/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const name = req.body.name
+  const email = req.body.email
+  const edited = {
+    name: name,
+    email: email
+  }
+
+  db.editUser(id, edited, req.app.get('connection'))
+    .then(() => {
+      res.redirect('/users')
+    })
+})
+
+router.get('/users/confirm/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db.getUser(id, req.app.get('connection'))
+    .then((result) => {
+      res.render('confirm', result[0])
+    })
+})
+
+router.post('/users/delete/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db.deleteUser(id, req.app.get('connection'))
+    .then(() => {
+      res.redirect('/users')
     })
 })
 
